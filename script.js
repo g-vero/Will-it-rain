6f289a64198ffc8af5f95972f27fbb93b868fe37
g-vero <gentiliveronica1@gmail.com>
2019-08-29 19:19:20 +0200

    Fetch

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
 
+    //Reset results
+
+    resultsBox.style.bottom = '0';
+    resultsBox.style.opacity = '0';
+
     //Get input field value(location)        
     let locationInput = document.getElementById('location');
     let location = locationInput.value;
@@ -18,6 +23,39 @@ btn.addEventListener('click', () => {
     if(location){
 
         //Api call
+
+        const cors = 'https://cors-anywhere.herokuapp.com/';
+        const apiKey = YOUR_API_KEY;
+        const url = `${cors}api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${apiKey}`;
+
+        fetch(url)
+            .then(res => {
+                if(res.ok){
+                    return res.json();
+                } else {
+                    throw Error(`Request rejected with status ${res.status}`);
+                }
+            
+            })
+            .then(data => {
+                //console.log(data);
+
+                const iconId = data.weather[0].icon;
+                    const iconUrl = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
+                    document.getElementById('icon').src = `${iconUrl}`;
+
+                    resultsBox.style.bottom = '5%';
+                    resultsBox.style.opacity = '1';
+
+                    city.innerHTML = `${data.name}`;
+                    desc.innerHTML = `${data.weather[0].description}`;
+                    temp.innerHTML = `${data.main.temp}°C`;
+
+                    locationInput.value = '';
+            })
+            .catch(err => {
+                console.log(err)
+            })
 
        
     } else {
